d578e0a8f1c828f9af43425c84ca69aa7b131a5c
David Härer <david@familie-haerer.de>
2022-10-08 22:20:55 +0200

    Squashed '.config/nvim/bundle/vim-doge/' content from commit d18866f10
    git-subtree-dir: .config/nvim/bundle/vim-doge
    git-subtree-split: d18866f10e83d388ab204d7c7bb531269e78386b

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+export enum Language {
+  PHP = 'php',
+  TYPESCRIPT = 'typescript',
+  PYTHON = 'python',
+  C = 'c',
+  CSHARP = 'cs',
+  CPP = 'cpp',
+  BASH = 'bash',
+  RUBY = 'ruby',
+  LUA = 'lua',
+  JAVA = 'java',
+  RUST = 'rust',
+}
