ed6c88f4e45670ae44bfc970f1064adbb8f84b56
David Härer <david@haerer.dev>
2023-11-14 22:56:49 +0100

    .config/wezterm/wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,15 @@
 local wezterm = require 'wezterm'
 
+local BLACK = "#0A0E14"
+local GRAY = "#4D5566"
+local WHITE = "#B3B1AD"
+local RED = "#FF3333"
+local ORANGE = "#E6B450"
+local YELLOW = "#FF8F40"
+local GREEN = "#C2D94C"
+local CYAN = "#95E6CB"
+local BLUE = "#59C2FF"
+local MAGENTA = "#FFEE99"
 
 return {
   color_scheme = "Ayu Dark (Gogh)",
@@ -14,4 +24,34 @@ return {
     font = wezterm.font { family = 'ComicCodeLigatures Nerd Font', weight = 'Bold' },
     font_size = 12.0,
   },
+
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+
+  colors = {
+    tab_bar = {
+      background = BLACK,
+      active_tab = {
+        bg_color = BLACK,
+        fg_color = YELLOW,
+        intensity = "Bold",
+      },
+      inactive_tab = {
+        bg_color = BLACK,
+        fg_color = GRAY,
+      },
+      inactive_tab_hover = {
+        bg_color = BLACK,
+        fg_color = WHITE,
+      },
+      new_tab = {
+        bg_color = BLACK,
+        fg_color = GRAY,
+      },
+      new_tab_hover = {
+        bg_color = BLACK,
+        fg_color = WHITE,
+      },
+    } 
+  }
 }
