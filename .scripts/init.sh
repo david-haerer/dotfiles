30a64e94f8d863bbaeb006763247c64ff5410649
David Härer <david@haerer.dev>
2025-02-28 21:05:38 +0100

    .scripts

--- .scripts/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-git config --global init.defaultBranch main
-git init --bare .dotfiles.git
-alias dot="git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME"
-dot config status.showUntrackedFiles no
-dot remote add origin git@github.com:david-haerer/dotfiles.git \
-    || dot remote add origin https://github.com/DavidHeresy/dotfiles.git
